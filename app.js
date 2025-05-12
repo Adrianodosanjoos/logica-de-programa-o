b3a861097981cd27d4fe3c90f6ae4839b206e69e
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-05-12 14:39:34 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-let numeroSecreto = 29;
-let chute = prompt('escolha um número entre 1 e 30');
+let numeroSecreto = 5;
+let chute;
+let tentativas = 1;
+
+
+while(chute != numeroSecreto) {
+
+    chute = prompt('escolha um número entre 1 a 30');
 
 if (numeroSecreto == chute) {
-    console.log('isso ai voce acertou');
+    alert(`isso ai voce acertou ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+
+    if(chute > numeroSecreto) {
+        alert(`O numero secreto é menor que ${chute} foi ${tentativas} tentativa`);
+    } else {
+        alert(`O numero secreto é maior que ${chute} foi ${tentativas} tentativa`);
+    }
+  // tentativas = tentativas + 1;
+  tentativas++;
 }
+
+} 
+
+
+
+
